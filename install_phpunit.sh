8a17b09cccb7c7823cf552b79ceaca2d6b0625c9
Fran Naranjo <fran.naranjo@gmail.com>
2012-09-20 21:29:21 +0200

    Created script that installs phpunit with dependencies from github. Dependencies are set as submodules

---
+++ install_phpunit.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+git clone git://github.com/sebastianbergmann/phpunit.git
+
+cd phpunit
+
+git submodule init;
+
+git submodule add git://github.com/sebastianbergmann/dbunit.git
+git submodule add git://github.com/sebastianbergmann/php-file-iterator.git
+git submodule add git://github.com/sebastianbergmann/php-text-template.git
+git submodule add git://github.com/sebastianbergmann/php-code-coverage.git
+git submodule add git://github.com/sebastianbergmann/php-token-stream.git
+git submodule add git://github.com/sebastianbergmann/php-timer.git
+git submodule add git://github.com/sebastianbergmann/phpunit-mock-objects.git
+git submodule add git://github.com/sebastianbergmann/phpunit-selenium.git
+git submodule add git://github.com/sebastianbergmann/phpunit-story.git
+git submodule add git://github.com/sebastianbergmann/php-invoker.git
